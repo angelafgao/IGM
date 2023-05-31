a9e60265a35819b6df6c6d97b1c709dc9d97c07a
angelafgao <angelagao28@gmail.com>
2023-05-31 10:08:39 -0700

    update scripts

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,4 +1,10 @@
-# denoising experiments
+# mnist denoising experiments
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 5 --dataset 'MNIST' --sigma 0.5 --image_size 64 --batchGD --class_idx 8 --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 35 --dataset 'MNIST' --sigma 0.5 --image_size 64 --batchGD --class_idx 8 --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 75 --dataset 'MNIST' --sigma 0.5 --image_size 64 --batchGD --class_idx 8 --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 150 --dataset 'MNIST' --sigma 0.5 --image_size 64 --batchGD --class_idx 8 --GMM_EPS 1e-3 --latent_type gmm
+
+# celeb denoising experiments
 python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 95 --dataset 'bond' --sigma 0.1 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
 python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 95 --dataset 'obama' --sigma 0.1 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
 
