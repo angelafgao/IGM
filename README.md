4eb0b9020147261d211557f6fd0fcc23e0f3bef3
angelafgao <angelagao28@gmail.com>
2023-05-31 10:07:03 -0700

    add test scripts

--- README.md
+++ README.md
@@ -7,19 +7,23 @@ Oscar Leong*,   Angela F. Gao*,   He Sun    Katherine L. Bouman
 ## Requirements
 General requirements for PyTorch release:
 * [pytorch](https://pytorch.org/)
-<!-- 
-For seismic tomography:
-* [eikonalfm](https://pypi.org/project/eikonalfm/) -->
-
-<!-- Please check ``` DeepGEM.def``` to build the singularity container.
 
 ## Citation
 ```
-@inproceedings{gao2021deepgem,
- author = {Gao, Angela F and Castillo, Jorge C and Yue, Yisong and Ross, Zachary E and Bouman, Katherine L},
- booktitle = {Advances in Neural Information Processing Systems},
- title = {DeepGEM: Generalized Expectation-Maximization for Blind Inversion},
- volume = {35},
- year = {2021}
+@inproceedings{gao2023image,
+  title={Image Reconstruction without Explicit Priors},
+  author={Gao, Angela F and Leong, Oscar and Sun, He and Bouman, Katherine L},
+  booktitle={ICASSP 2023-2023 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
+  pages={1--5},
+  year={2023},
+  organization={IEEE}
 }
-``` -->
+```
+
+```
+@article{leong2023ill,
+      title={Ill-Posed Image Reconstruction Without an Image Prior},
+      author={Leong, Oscar and Gao, Angela F and Sun, He and Bouman, Katherine L},
+      journal={arXiv preprint arXiv:2304.05589},
+      year={2023}}
+```
---
+++ test_scripts.sh
@@ -0,0 +1,13 @@
+# denoising experiments
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 95 --dataset 'bond' --sigma 0.1 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'denoising' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 95 --dataset 'obama' --sigma 0.1 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+
+
+# black hole compressed sensing experiments with fixed noise value for all frequencies
+python igm_multi_inv_learning_new.py --task 'compressed-sensing' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 60 --dataset 'sagA' --sigma 2 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'compressed-sensing' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 60 --dataset 'm87' --sigma 2 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'compressed-sensing' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 60 --dataset 'sagA_video' --sigma 2 --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+
+# realistic noise scenario for black hole compressed sensing
+python igm_multi_inv_learning_new.py --task 'compressed-sensing' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 60 --dataset 'sagA_video' --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
+python igm_multi_inv_learning_new.py --task 'compressed-sensing' --latent_dim 40 --generator_type 'deepdecoder' --num_imgs 60 --dataset 'm87' --image_size 64 --batchGD  --GMM_EPS 1e-3 --latent_type gmm
